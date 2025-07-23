11db9b5ae33ee0fcf5a561d1b35c54fabedcc09b
Ewan-Dev <ewanmccairn2010@gmail.com>
2025-07-23 21:46:04 +0100

    feat(media): add language select to video and song pages

--- frontend/src/routes/music/+page.svelte
+++ frontend/src/routes/music/+page.svelte
@@ -2,25 +2,46 @@
     import { supabase } from "../../../lib/supabaseClient";
     import Sidebar from "../../../lib/Sidebar.svelte"; 
     import MediaBox from "../../../lib/MediaBox.svelte";
-    let language = 'zh'; // Default language for music for now until user selection function is implemented
-    let songs = [] // List to store fetched music data
+    import { onMount } from "svelte";
 
+    let songs = $state([]) // List to store fetched music data
+    let availableLanguages = $state([]) // List to store fetched languages available
+    let currentLanguage = $state("") // Current language to fetch media
+    
     // Fetch music data from Supabase
-    async function fetchMusic() {
+    async function fetchMusic(lang) {
         const { data, error } = await supabase
             .from('media') // From the 'media' table
             .select('*') // Select all columns
-            .eq('language', language) // Filter by language
+            .eq('language', lang) // Filter by language
             .eq('media_type', 'music'); // Filter by media type
         if (!error) {
             songs = data || []; // If data is returned, assign it to songs
+            console.log(data)
         } else {
             console.error('Error fetching music:', error);
         }
     }
+    async function fetchLanguages(){
+        const {data, error} = await supabase
+            .from("languages")
+            .select("*")
+        if(data){
+            availableLanguages = data
+            console.log(availableLanguages)
+            return data
+        }
+        if (error){
+            throw error
+    }
+}
+    onMount(() => {
+        fetchLanguages()
+    })
 
-    fetchMusic();
-
+    $effect(() => {
+         fetchMusic(currentLanguage)
+    })
 </script>
 
 <main class="route">
@@ -29,25 +50,41 @@
     <h1 class="page-header">Music</h1>
         <!-- Main content section for styles to be applied -->
     <section class="main-content">
+    <select bind:value={currentLanguage}>
+        {#each availableLanguages as language}
+            <option value={language.short}>{language.emoji} {language.name}</option>
+        {/each}
+    </select>
+    <section class="media-container">
         <!-- For each song, create a MediaBox component -->
-    {#each songs as song} 
-    <MediaBox 
-        name={song.name} 
-        artist={song.artist} 
-        cover={song.cover} 
-        level={song.level} 
-        rating={song.rating}
-        link={`#/app/music/${song.id}`} />
+        {#each songs as song} 
+        <MediaBox 
+            name={song.name} 
+            artist={song.artist} 
+            cover={song.cover} 
+            level={song.level} 
+            rating={song.rating}
+            link={`#/app/music/${song.id}`} />
     {/each}
+    </section>
     </section>
     </section>
 </main>
 
 <style>
-    main{
+   main{
         display: flex;
         gap: 1em;
         align-items: flex-start;
     }
-
+    .main-content{
+        display: flex;
+        flex-direction: column;
+    }
+    .media-container{
+        display: flex;
+        gap: 1em;
+        flex-wrap: wrap;
+        width: 100%;
+    }
 </style>
