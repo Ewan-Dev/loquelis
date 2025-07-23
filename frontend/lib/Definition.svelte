ef96bc28e889be2b7e39621617afbb792dfc150e
Ewan-Dev <ewanmccairn2010@gmail.com>
2025-07-24 00:36:07 +0100

    feat(flashcards): add info dialog and make flashcard deck fetch reactive - Added flashcardDecks to be set as flashcard decks fetch from Supabase - Site now has a dialog to inform users on why their deck may not show up - (It is because only flashcard decks with language set to current word show)

--- frontend/lib/Definition.svelte
+++ frontend/lib/Definition.svelte
@@ -4,10 +4,11 @@
     import InlineStatus from "./InlineStatus.svelte";
     let {definition, word, partOfSpeech, romanisation, language} = $props() // Pass 
     const username = "samwu" // Example username before I add auth
-    let definitionStatus = $state({message: "", type: ""})
-    let currentDeckID = $state()
-    let flashcardDecks = $state([])
-    // Fetch current flashcards in deck
+    let definitionStatus = $state({message: "", type: ""}) // Object to store status from ayncronous request
+    let currentDeckID = $state() // Current dec ID
+    let flashcardDecks = $state([]) // Stores flashcard decks
+    let infoDialog = $state(HTMLObjectElement) // The dialog box with deck info
+    // Fetch current flashcards lin deck
     async function fetchFlashcardData(id){
         const { data, error } = await supabase
             .from("flashcards")
@@ -50,11 +51,12 @@
         }
         return data
     } 
-        onMount( async () => {
+        $effect( async () => {
             flashcardDecks = await fetchUserFlashcardDecks(username) 
+            console.log(language)
             console.log(flashcardDecks)
         })
-        $inspect(language)
+
 
 </script>
 
@@ -68,20 +70,36 @@
         <p class="part-of-speech">{partOfSpeech}</p>
         <p>{definition}</p>
         {#if flashcardDecks}
-        <span class="flashcard-upload">
-            <button on:click={() => updateFlashcard(currentDeckID)}><span class="material-symbols-rounded">library_add</span></button>
-            <select bind:value={currentDeckID}>
-                {#each flashcardDecks as flashcardDeck}
-                <option value={flashcardDeck.id}>{flashcardDeck.name}</option>
-                {/each}
-            </select>
+        <span class="flashcard-functions">
+            <span class="flashcard-upload">
+                <button class="add-to-deck flashcard-btn" on:click={() => updateFlashcard(currentDeckID)}><span class="material-symbols-rounded">library_add</span></button>
+                <select bind:value={currentDeckID}>
+                    {#each flashcardDecks as flashcardDeck}
+                    <option value={flashcardDeck.id}>{flashcardDeck.name}</option>
+                    {/each}
+                </select>
+            </span>
+             <button class="info-flashcards-btn flashcard-btn" on:click={infoDialog.showModal()} ><span class="material-symbols-rounded">info</span> <!--Opens dialog/modal box on button click-->
+  
+            </button>
         </span>
-        {/if}
+         <dialog class="info-flashcards-container" bind:this={infoDialog}>
+             <strong class='definition-info-header'>Where's my deck?</strong>
+            <p class='definition-info-body'>This is as you may not yet have a deck for the language you are curretly studying; You can only add words to decks of the same language</p>
+            <button on:click={infoDialog.close()} class="close-dialog">OK</button> <!--Closes the dialog box with info about decks-->
+        </dialog>
             <InlineStatus type={definitionStatus.type} message={definitionStatus.message}/>
-    </section>
-{/if}
 
+{/if}
+        </section>
+{/if}
 <style>
+
+    /**
+    TODO:
+    - Style dialog box
+    */
+
     section{
         height: fit-content;
         width: 12.5em;
@@ -91,7 +109,6 @@
         border-radius: 1em;
         z-index: 1;
     }
-
 
     p{
         margin: 0.2em 0;
@@ -119,7 +136,7 @@
     select{
         height: 1.5em;
     }
-    button{
+    .flashcard-btn{
         width: 1.5em;
         text-align: center;
         font-size: 1em;
@@ -133,10 +150,11 @@
     }
     span{
         font-size: 1em;
-        text-align: center;
-         display: flex;
-        flex-wrap: wrap;
-        gap:0.25em;
+        text-align: left;
+    }
+    dialog{
+        width: 17.5em;
+        border-radius: 16px;
     }
     .flashcard-upload{
         height: fit-content;
@@ -151,10 +169,25 @@
     .material-symbols-rounded{
         font-size: 0.95em;
     }
-
-    button:hover{
+    .flashcard-functions{
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        gap:0.25em;
+    }
+    .add-to-deck:hover{
         background-color: #558cea;
         color: #ffffff;
         border-color: #3b69e8;
     }
+    .close-dialog {
+        height: 1.75em;
+        width: 5em;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        border: #545454 2px solid;
+    }
+
 </style>
