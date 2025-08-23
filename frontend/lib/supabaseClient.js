03da2a94e3ee0f9b1b3db5b600ff56df6e473ae0
Ewan-Dev <ewanmccairn2010@gmail.com>
2025-08-24 00:19:28 +0800

    feat(supabaseClient): change supabaseClient to use .env for keys - For custom user customisability

--- frontend/lib/supabaseClient.js
+++ frontend/lib/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://ibgkltqlgbafpoljizdu.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliZ2tsdHFsZ2JhZnBvbGppemR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMjU1NzksImV4cCI6MjA2NDkwMTU3OX0.Awa-Gdd3b4uR_1MnQySyA5s6hfVfyZB6jlVS1Jj_Afk"
+// Added from .env so add your own!
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
